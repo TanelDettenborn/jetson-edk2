690e64b468e407f040367bc26d3fb3204d44b4fd
Jörg Thalheim <joerg@thalheim.io>
2024-08-29 16:52:54 +0000

    simplify cross build shell

--- shell.nix
+++ shell.nix
@@ -3,54 +3,29 @@
   nixpkgs ? fetchTarball "https://github.com/NixOS/nixpkgs/archive/a6292e34000dc93d43bccf78338770c1c5ec8a99.tar.gz",
 }:
 let
-  pkgs = import nixpkgs { };
-  cross = import nixpkgs {
-    crossSystem = {
-      config = "aarch64-linux-gnu";
-    };
-  };
-
-  python = pkgs.python3.override {
-    self = python;
-    packageOverrides = pyfinal: pyprev: {
-      edk2-pytools-ext = pyfinal.callPackage ./edk2-pytool-extensions.nix { };
-      # NOTE: Error inside basetool.nix file!
-      # edk2-basetools = pyfinal.callPackage ./edk2-basetools.nix { };
-    };
-  };
-
-  my-uefi36-3-gcc-tools = pkgs.linkFarm "uefi36-3-gcc-tools" [
-    {
-      name = "bin/aarch64-linux-gnu-gcc-ar";
-      path = "${cross.buildPackages.libgcc}/bin/aarch64-linux-gnu-gcc-ar";
-    }
-    {
-      name = "bin/aarch64-linux-gnu-gcc";
-      path = "${cross.buildPackages.gcc12}/bin/aarch64-linux-gnu-gcc";
-    }
-    {
-      name = "bin/aarch64-linux-gnu-objcopy";
-      path = "${cross.buildPackages.gcc12}/bin/aarch64-linux-gnu-objcopy";
-    }
-  ];
+  pkgs = import nixpkgs { crossSystem.config = "aarch64-linux-gnu"; };
+  buildPackages = pkgs.buildPackages;
+  python = buildPackages.python3.withPackages (ps: [
+    ps.edk2-pytool-library
+    (ps.callPackage ./edk2-pytool-extensions.nix { })
+    # NOTE: Error inside basetool.nix file!
+    #(ps.callPackage ./edk2-basetools.nix { })
+    # python-pkgs.edk2-basetools
+    ps.regex
+    ps.kconfiglib
+  ]);
 in
 pkgs.mkShell {
-  packages = [
-    pkgs.nuget
-    pkgs.mono
-    pkgs.libuuid
-    pkgs.gcc12 # TODO: Needed?
-    pkgs.dtc
-    pkgs.acpica-tools
-    pkgs.lcov
-    my-uefi36-3-gcc-tools
+  depsBuildBuild = [
 
-    (python.withPackages (python-pkgs: [
-      python-pkgs.edk2-pytool-library
-      python-pkgs.edk2-pytools-ext
-      # python-pkgs.edk2-basetools
-      python-pkgs.regex
-      python-pkgs.kconfiglib
-    ]))
+  ];
+  NUGET_PATH = pkgs.lib.getExe buildPackages.nuget;
+  nativeBuildInputs = [
+    buildPackages.libuuid
+    buildPackages.dtc
+    buildPackages.acpica-tools
+    python
+    # FIXME: needs cross fix
+    #pkgs.buildPackages.lcov
   ];
 }
